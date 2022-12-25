9e06cdeb737ef1983bbb9c2c37f2831b3dc07dea
LayaArianpour <l.aryanpour80@gmail.com>
2022-12-25 23:21:34 +0330

    Load Image

--- react-getimage/src/App.js
+++ react-getimage/src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState,useEffect} from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Container,Row,Card} from 'react-bootstrap'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  const[image,setImage]=useState([])
+
+  useEffect(()=>{
+    
+    async function loadImage(){
+      var imageparams={
+        method:'GET',
+        headers:{
+          'Content-Type':'application/json'
+        },
+      }
+
+      var getImage=await fetch('https://random-d.uk/api/random',imageparams)
+      .then(Response=>Response.json())
+      .then(data=>{
+        console.log(data.url)
+        setImage(data)
+      })
+
+    }
+
+    loadImage()
+  })
+
+  return(
+    <Container className='m-auto'>
+      <Row className='col-10'>
+        <Card>
+          <Card.Img src={image.url} />
+        </Card>
+      </Row>
+    </Container>
   );
 }
 
